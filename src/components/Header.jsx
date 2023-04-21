2105dee4d5a030f8ad777795cf55424d9a3781ff
Bianca Niemann <payrollsollutions@gmail.com>
2023-04-21 12:15:23 +0200

    home page media settings

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../images/shared/logo.svg"
+import hamburger from "../images/shared/icon-hamburger.svg"
 
 export default function Header() {
     return (
         <header className="flex header">
             <img className="logo" src={logo} />
+            <img className="hamburger" src={hamburger}/>
             <nav className="flex nav-bar">
                 <ul className="primary-navigation underline-indicators flex ">
-                    <Link to="/" className="underline uppercase text-white letter-spacing-2"><span>00</span>Home</Link>
-                    <Link to="destination" className="underline uppercase text-white letter-spacing-2"><span>01</span>Destination</Link>
-                    <Link to="crew" className="underline uppercase text-white letter-spacing-2"><span>02</span>Crew</Link>
-                    <Link to="technology" className="underline uppercase text-white letter-spacing-2"><span>03</span>Technology</Link>
+                    <Link to="/" className="underline uppercase text-white letter-spacing-2"><span className="number">00</span>Home</Link>
+                    <Link to="destination" className="underline uppercase text-white letter-spacing-2"><span className="number">01</span>Destination</Link>
+                    <Link to="crew" className="underline uppercase text-white letter-spacing-2"><span className="number">02</span>Crew</Link>
+                    <Link to="technology" className="underline uppercase text-white letter-spacing-2"><span className="number">03</span>Technology</Link>
                 </ul>
             </nav>
         </header>
